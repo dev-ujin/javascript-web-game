d5a34ba981229ff313683ff2cc613793842b6756
YouJin Lee <dev.ujin518@gmail.com>
2021-01-23 21:50:17 +0100

    :sparkles: Make tic-tac-toe

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -2,12 +2,60 @@ var body = document.body;
 var table = document.createElement("table");
 var rowArray = [];
 var colArray = [];
+var turn = "X";
+var result = document.createElement("div");
 
 var clickHandler = function(event) {
-    console.log(event.target);
-    console.log(event.target.parentNode);
-    console.log(event.target.parentNode.parentNode);
-}
+    var row = rowArray.indexOf(event.target.parentNode);
+    var col = colArray[row].indexOf(event.target);
+    console.log(row, col);
+
+    if (colArray[row][col].textContent !== "") {
+        console.log("빈칸이 아닙니다!");
+    }
+    else {
+        console.log("빈칸입니다!");
+        colArray[row][col].textContent = turn;
+        var success = false;
+        // 가로줄 검사
+        if (colArray[row][0].textContent === turn && colArray[row][1].textContent === turn &&colArray[row][2].textContent === turn) {
+            success = true;
+        }
+        // 세로줄 검사
+        if (colArray[0][col].textContent === turn && colArray[1][col].textContent === turn && colArray[2][col].textContent === turn) {
+            success = true;
+        }
+        // 대각선 검사
+        if (row - col === 0) {
+            if (colArray[0][0].textContent === turn && colArray[1][1].textContent === turn && colArray[2][2].textContent === turn) {
+                success = true;
+            }
+        }
+        if (Math.abs(row - col) === 2) {
+            if (colArray[0][2].textContent === turn && colArray[1][1].textContent === turn && colArray[2][0].textContent === turn) {
+                success = true;
+            }
+        }
+        console.log(success);
+        if (success) {
+            result.textContent = turn + " wins!";
+            turn = "X";
+            colArray.forEach(function (row) {
+                row.forEach(function (col) {
+                    col.textContent = "";
+                });
+            });
+        }
+        else {
+            if (turn === "X") {
+                turn = "O";
+            }
+            else {
+                turn = "X";
+            }
+        }
+    }
+};
 
 for (var i=1 ; i<=3 ; i+=1) {
     var tr = document.createElement("tr");
@@ -22,4 +70,4 @@ for (var i=1 ; i<=3 ; i+=1) {
     table.appendChild(tr);
 }
 body.appendChild(table);
-
+body.appendChild(result);
