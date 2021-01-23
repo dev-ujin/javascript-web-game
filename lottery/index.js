249b5112125e83dcc779c41899b46e48ad478c59
YouJin Lee <dev.ujin518@gmail.com>
2021-01-24 00:59:12 +0100

    :hammer: Functionalize code

--- lottery/index.js
+++ lottery/index.js
@@ -1,10 +1,5 @@
 // fill을 하면 empty->undefined. empty는 반복문 불가!
-var nums = Array(45).fill().map(function(num, index) {
-    return index+1;
-});
-/* nums.forEach(function (num, index) {
-    nums[index] = index+1;
-});*/
+var nums = Array(45).fill().map(function(num, index) {return index+1;});
 
 var shuffle = [];
 while (nums.length > 0) {
@@ -12,92 +7,61 @@ while (nums.length > 0) {
 }
 
 var bonus = shuffle[shuffle.length - 1];
-var selectedNums = shuffle.slice(0, 6);
-console.log(bonus, selectedNums.sort(function (p,c) {return p-c;}));
+var selectedNums = shuffle.slice(0, 6).sort(function (p,c) {return p-c;});
+var resultNums = document.querySelector("#resultNums");
 
-var resultNums = document.getElementById("resultNums");
-
-
-setTimeout(function handler() {
+function paintBall(num, resultSection) {
     var ball = document.createElement("div");
-    ball.textContent = selectedNums[0];
+    ball.textContent = num;
     ball.style.display = "inline-block";
     ball.style.border = "1px solid black";
     ball.style.borderRadius = "10px";
     ball.style.width = "20px";
     ball.style.height = "20px";
     ball.style.textAlign = "center";
-    resultNums.appendChild(ball);
+    ball.style.marginRight = "10px";
+    ball.style.fontSize = "15px";
+    ball.id = "ballId" + num;
+    var bgColor;
+    if (num <= 10) {
+        bgColor = "red";
+    }
+    else if (num <=20) {
+        bgColor = "orange";
+    }
+    else if (num <= 30) {
+        bgColor = "yellow";
+    }
+    else if (num <= 40) {
+        bgColor = "blue";
+    }
+    else {
+        bgColor = "green";
+    }
+    ball.style.background = bgColor;
+    resultSection.appendChild(ball);
+}
+
+setTimeout(function handler() {
+    paintBall(selectedNums[0], resultNums)
 }, 1000);
 setTimeout(function handler() {
-    var ball = document.createElement("div");
-    ball.textContent = selectedNums[1];
-    ball.style.display = "inline-block";
-    ball.style.border = "1px solid black";
-    ball.style.borderRadius = "10px";
-    ball.style.width = "20px";
-    ball.style.height = "20px";
-    ball.style.textAlign = "center";
-    resultNums.appendChild(ball);
+    paintBall(selectedNums[1], resultNums)
 }, 2000);
 setTimeout(function handler() {
-    var ball = document.createElement("div");
-    ball.textContent = selectedNums[2];
-    ball.style.display = "inline-block";
-    ball.style.border = "1px solid black";
-    ball.style.borderRadius = "10px";
-    ball.style.width = "20px";
-    ball.style.height = "20px";
-    ball.style.textAlign = "center";
-    resultNums.appendChild(ball);
+    paintBall(selectedNums[2], resultNums)
 }, 3000);
 setTimeout(function handler() {
-    var ball = document.createElement("div");
-    ball.textContent = selectedNums[3];
-    ball.style.display = "inline-block";
-    ball.style.border = "1px solid black";
-    ball.style.borderRadius = "10px";
-    ball.style.width = "20px";
-    ball.style.height = "20px";
-    ball.style.textAlign = "center";
-    resultNums.appendChild(ball);
+    paintBall(selectedNums[3], resultNums)
 }, 4000);
 setTimeout(function handler() {
-    var ball = document.createElement("div");
-    ball.textContent = selectedNums[4];
-    ball.style.display = "inline-block";
-    ball.style.border = "1px solid black";
-    ball.style.borderRadius = "10px";
-    ball.style.width = "20px";
-    ball.style.height = "20px";
-    ball.style.textAlign = "center";
-    resultNums.appendChild(ball);
+    paintBall(selectedNums[4], resultNums)
 }, 5000);
 setTimeout(function handler() {
-    var ball = document.createElement("div");
-    ball.textContent = selectedNums[5];
-    ball.style.display = "inline-block";
-    ball.style.border = "1px solid black";
-    ball.style.borderRadius = "10px";
-    ball.style.width = "20px";
-    ball.style.height = "20px";
-    ball.style.textAlign = "center";
-    resultNums.appendChild(ball);
+    paintBall(selectedNums[5], resultNums)
 }, 6000);
-/*for (var i = 0 ; i < selectedNums.length ; i += 1) { 
-    var ball = document.createElement("div");
-    ball.textContent = selectedNums[i];
-    resultNums.appendChild(ball);
-}*/
+
 setTimeout(function handler() {
-    var resultBonus = document.getElementsByClassName("resultBonus")[0];
-    var bonusBall = document.createElement("div");
-    bonusBall.textContent = bonus;
-    ball.style.display = "inline-block";
-    ball.style.border = "1px solid black";
-    ball.style.borderRadius = "10px";
-    ball.style.width = "20px";
-    ball.style.height = "20px";
-    ball.style.textAlign = "center";
-    resultBonus.appendChild(bonusBall);
+    var resultBonus = document.querySelector(".resultBonus");
+    paintBall(bonus, resultBonus)
 }, 7000);
